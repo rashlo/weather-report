2d067a8a4c60fb361a87067b753ff5da9ca2e50b
Rasheed Lochan <rasheed_l@outlook.com>
2021-04-18 13:39:54 -0400

    deployment 6

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     country: undefined,
     humidity: undefined,
     description: undefined,
-    //code: undefined,
+    code: undefined,
     icon: undefined,
     error: undefined
   }
